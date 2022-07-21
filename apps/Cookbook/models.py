282a1a2945eebcad7de00cc71ae3f969963f6d42
mrdefkit <balazs.csutar@gmail.com>
2022-07-21 13:45:04 +0200

    juk 21

--- apps/Cookbook/models.py
+++ apps/Cookbook/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    Image = models.CharField(max_length=30)
+    Name = models.CharField(max_length=30)
+    
+    Category = models.CharField(max_length=30)
+    SubCategory = models.CharField(max_length=30)
+    
+    Quantity = models.CharField(max_length=30)
+    Frequency = models.CharField(max_length=30)
+    Tags = models.CharField(max_length=30)
+        
+    PurchaseDate = models.DateField()
--- static/javascript/cookbook.js
+++ static/javascript/cookbook.js
@@ -46,6 +46,51 @@ jQuery(function($) {
 
 });
 
+jQuery(function($) {
+  'use strict';
+
+  // -------------------------------------------------------------
+  //   Basic Navigation
+  // -------------------------------------------------------------
+  (function() {
+    var $frame = $('#basic2');
+    // Call Sly on frame
+    $frame.sly({
+      horizontal: 1,
+      itemNav: 'basic',
+      smart: 1,
+      activateOn: 'click',
+      mouseDragging: 1,
+      touchDragging: 1,
+      releaseSwing: 1,
+      startAt: 0,
+      scrollBy: 1,
+      activatePageOn: 'click',
+      speed: 300,
+      elasticBounds: 1,
+      easing: 'easeOutExpo',
+      dragHandle: 1,
+      dynamicHandle: 1,
+      clickBar: 1,
+
+    });
+
+  }());
+
+  // -------------------------------------------------------------
+  //   Centered Navigation
+  // -------------------------------------------------------------
+
+  $(".clearfix li").click(function(){
+
+    var audio = new Audio('static/sounds/Menu.wav');
+    audio.play();
+
+
+});
+
+});
+
 $(".UI_All_Tags_Recepies_Tag").click(function(){
 
   var audio = new Audio('static/sounds/Click.wav');
@@ -63,6 +108,11 @@ $(document).ready(function(){
     k = i - 1;
 
     $(".UI_All_Container_Page").not( ":nth-child(" + k + ")" ).css({'left': '-5000px','opacity':'0'});
+
+    if(k == 1)
+    {
+      $(".UI_All_Container_Page").eq(0).css({'left': '-5000px','opacity':'0'});
+    }
 
     if(k == 2)
     {
